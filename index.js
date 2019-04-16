8b1aa6f35159630f53beeeab15d2eae294c89da4
Murilo Mac <m4k2005@yahoo.com>
2019-04-16 03:38:58 -0300

    Puttin index file outside src directory.

---
+++ index.js
@@ -0,0 +1,7 @@
+import './src/index.css';
+
+export { default as Theme } from './src/Theme';
+export { default as Atelier } from './src/Atelier';
+export { default as Button } from './src/Button';
+export { default as Divider } from './src/Divider';
+export { Container, Row, Col } from './src/Flexbox';
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css';
-
-export { default as Theme } from './Theme';
-export { default as Atelier } from './Atelier';
-export { default as Button } from './Button';
-export { default as Divider } from './Divider';
-export { Container, Row, Col } from './Flexbox'; 
