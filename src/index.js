6b3140f54194d312a3e0714f677bc0dd7f97f465
Murilo Mac <m4k2005@yahoo.com>
2019-04-16 03:31:50 -0300

    Adding a correct main entry with an index file who export some components.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import './index.css';
 
 export { default as Theme } from './Theme';
 export { default as Atelier } from './Atelier';
+export { default as Button } from './Button';
+export { default as Divider } from './Divider';
+export { Container, Row, Col } from './Flexbox'; 
