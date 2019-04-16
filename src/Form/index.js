ec2d0eea1526228c6423f2e92294a6f2985de621
Murilo Mac <m4k2005@yahoo.com>
2019-04-16 03:57:56 -0300

    Exporting all components over index file.

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
 export { default as Text } from './Text';
 export { default as Pin } from './Pin';
-export { default as List } from './List';
+export { default as FormList } from './List';
 export { default as Checkbox } from './Checkbox';
