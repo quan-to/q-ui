8c8000fc02a692ea90742e0a6290d70b29fb7dc8
Caio Alcantara <alcantaracaiolucas@gmail.com>
2019-04-24 02:16:06 -0300

    chore: Update webpack config and gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,28 @@
 const path = require('path');
 
 module.exports = {
+	mode: 'production',
+	entry: './src/index.js',
 	output: {
-		filename: '[name].js',
+		path: path.join(__dirname, 'lib'),
+		filename: 'q-ui.min.js',
 		libraryTarget: 'umd',
+		library: 'q-ui'
 	},
 	module: {
 		rules: [
 			{
+				loader: 'babel-loader',
 				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				},
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,
+				use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
 			}
-		],
+		]
+	},
+	devServer: {
+		contentBase: [path.join(__dirname, 'src'), path.join(__dirname, 'lib')]
 	}
-}
+};
