262b96383b95b78f7b01a99be6cfad7b950a8e92
Murilo Mac <m4k2005@yahoo.com>
2019-04-30 19:50:24 -0300

    Fixing Logo component's height size.

--- src/Logo/index.js
+++ src/Logo/index.js
@@ -4,7 +4,7 @@ import styled from '@emotion/styled';
 const Logo = (props) => {
   const StyledLogo = styled.div`
     width: ${props.size}px;
-    height: ${(props.size + (props.size * 0.3))}px;
+    min-height: ${props.size}px;
     background: url(/assets/images/logo.png) no-repeat;
     background-size: 100% auto;
     margin: 0 auto;
