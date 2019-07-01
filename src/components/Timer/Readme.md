10b9e9fc4234ab3b324cb243fa86aec4e3f6d4a9
Murilo Mac <m4k2005@yahoo.com>
2019-07-01 18:08:41 -0300

    Fixing Timer reset feature.

---
+++ src/components/Timer/Readme.md
@@ -0,0 +1,8 @@
+React component example:
+
+```jsx
+<Timer
+  seconds={2}
+  resetable="Resetar Timer"
+>Game Over</Timer>
+```
--- src/components/Timer/index.js
+++ src/components/Timer/index.js
@@ -1,10 +1,18 @@
 import React, { PureComponent } from 'react';
+import styled from '@emotion/styled';
+import Button from './../Button';
 import If from './../If';
+import _theme from './../Theme';
 
 class Timer extends PureComponent {
   state = {
     seconds: 0,
     timer: null,
+  }
+
+  constructor(props) {
+    super(props);
+    this.reset = this.reset.bind(this);
   }
 
   toHHMMSS(input) {
@@ -32,25 +40,53 @@ class Timer extends PureComponent {
         }))
       }, 1000),
     });
-
   }
 
   componentWillUnmount() {
     clearInterval(this.state.timer);
   }
 
+  reset() {
+    this.setState({
+      seconds: this.props.seconds,
+      timer: setInterval(() => {
+        this.setState(prevState => ({
+          seconds: prevState.seconds - 1,
+        }))
+      }, 1000),
+    });
+  }
+
   render() {
+    const StyledButton = styled.button`
+      color: ${_theme.palette.blue};
+      border: none;
+      background: none;
+      padding: 0;
+      font-size: 16px;
+      text-decoration: underline;
+      margin-top: ${_theme.rythm.base}px;
+    `;
+
     if (this.state.seconds === 0) {
       clearInterval(this.state.timer);
     }
 
     return(
-      <div data-reset={this.props.reset}>
+      <div>
         <If test={this.state.seconds > 0}>
           <div>{this.toHHMMSS(this.state.seconds)}</div>
         </If>
         <If test={this.state.seconds === 0}>
           {this.props.children}
+
+          <If test={this.props.resetable}>
+            <div s>
+            <StyledButton onClick={() => this.reset()}>
+              {this.props.resetable}
+            </StyledButton>
+            </div>
+          </If>
         </If>
       </div>
     );
