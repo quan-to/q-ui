cbe505ec7048b137416953ecf8dd28ec104599d6
Rafael Dantas <rafael.dantas.br@gmail.com>
2019-07-17 18:42:41 -0300

    refactor(avatar): Improve style and logic
    Improve logic to have less code and add new style properties to make it easier to use

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -6,14 +6,18 @@ const Avatar = (props) => {
     width: ${props.size || '80'}px;
     border-radius: ${props.Circle ? '50%' : '0'};
     background-color: #9B9B9B;
-    box-shadow: 0px 0px 8px rgba(0, 0, 0, .1)
-  `
-  return (
-    <div>
-      <StyledAvatar className={props.className} src={ `${props.photo || '../../assets/icons/avatar-icon.svg'}` } />
-    </div>
-  )
-}
+    display: block;
+    margin: 27px auto;
+    box-shadow: 0px 0px 8px rgba(0, 0, 0, .1);
+  `;
 
+  const sourcePhoto = props.photo || '../../assets/icons/avatar-icon.svg';
+  return (
+    <StyledAvatar 
+      className={props.className} 
+      src={ sourcePhoto } 
+    />
+  );
+}
 
 export default Avatar;
