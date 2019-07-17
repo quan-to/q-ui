724c90cc241239399e7977f67eb0fd75b79a4625
Rafael Dantas <rafael.dantas.br@gmail.com>
2019-07-17 18:54:16 -0300

    improve(avatar-bg-color): Add bg color from color pallete
    Add grey from color pallete available in Q-UI

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import _theme from './../Theme';
 
 const Avatar = (props) => {
   const StyledAvatar = styled.img`
     width: ${props.size || '80'}px;
     border-radius: ${props.Circle ? '50%' : '0'};
-    background-color: #9B9B9B;
+    background-color: ${_theme.palette.regularGrey};
     display: block;
     margin: 27px auto;
     box-shadow: 0px 0px 8px rgba(0, 0, 0, .1);
